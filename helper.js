9c8c8c0ebe6d1b29bda07c084b04221afef7b6e9
Vladislav <vlad.dalechin@gmail.com>
2018-12-16 19:33:47 +0200

    erase unnecessary files

--- helper.js
+++
@@ -1,9 +0,0 @@
-function random(low, high) {
-    return Math.random() * (high - low) + low;
-}
-
-Array.prototype.remove = function (from, to) {
-    var rest = this.slice((to || from) + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
-};
